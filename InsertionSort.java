205c01856b729287c1d6e9eb65caead2ba401d8a
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2018-03-25 15:03:13 -0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,7 +1,23 @@
 public class InsertionSort {
 
-	public static void sort(ArrayBasedList<Product> plist) {
-        //TODO: Implement stable in-place insertion sort
-    }
-	
+	public static void sort(ArrayBasedList<Product> plist) throws Exception {
+		Product standardProduct = null, targetProduct = null;
+		int listSize = plist.size();
+		int insertIndex = 0;
+		boolean flag;
+		for (int i = 1; i < listSize; i++) {
+			flag = false;
+			standardProduct = plist.get(i);
+			for (int j = i - 1; j >= 0; j--) {
+				targetProduct = plist.get(j);
+				if (standardProduct.getAvgRating().floatValue() < targetProduct.getAvgRating().floatValue()) {
+					insertIndex = j;
+					flag = true;
+				}
+			}
+			if (flag) {
+				plist.swapValue(insertIndex, i);
+			}
+		}
+	}
 }
