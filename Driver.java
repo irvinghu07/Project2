346ac55d52e65e84e6c0554680846b1a873d900b
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2018-03-25 15:04:57 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,30 +4,65 @@ import java.lang.reflect.InvocationTargetException;
 public class Driver {
 
 	public static void main(String[] args) {
-		ArrayBasedList<Product> plist = new ArrayBasedList<Product>();
-		//TODO: Read .csv file from command line and parse into ArrayBasedList<Product>
-		// You should not need to declare another ArrayBasedList<Product> in main
-		
+		/* Check if arguments are valid */
+		/* program end with -1 if, arguments are invalid */
+		if (args.length != 1) {
+			System.out.println("Invalid Argument(s) Inputed!");
+			System.exit(-1);
+		}
+		ArrayBasedList<Product> plist = null;
+		/* parse content of file into an ArrayBasedList<Product> plist */
+		try {
+			plist = FileParser.parse(args[0]);
+			InsertionSort.sort(plist);
+		} catch (IOException e3) {
+			e3.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		/*
+		 * check if plist is still null, in order to avoid null pointer
+		 * exception
+		 */
+		/* program end with -2 if, plist is null */
+		if (plist == null) {
+			System.out.println("Something wrong while parsing the file");
+			System.exit(-2);
+		} else {
+			try {
+				ResultWriter.writeResult(plist, args[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("every thing is fine now!!!");
+			// System.exit(7);
+		}
+
 		try {
 			// This will test the certain edge case and accuracy of your sorts.
-			// This is NOT comprehensive and your own tests need to done in 
+			// This is NOT comprehensive and your own tests need to done in
 			// addition to this test.
 			//
-			// TODO: To look the timing of your sorts, uncomment printTiming in the 
-			// Test.java file. Large samples will take a long time to complete though.
+			// Test.java file. Large samples will take a long time to complete
+			// though.
 			Test.test(plist, args);
-		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | 
-				IllegalArgumentException | InvocationTargetException | IOException e) {
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | IOException e) {
 			System.out.println("Test could not run/finish.");
 		}
-		
-		//TODO: Sort based on averageRating from highest to lowest.
-		
-		//TODO: Write to a new .csv file named whatever the file being read is with "_sorted"
-		//      added to it. So if the file being read is called ratings_Stuff.csv, your file
-		//      will be called ratings_Stuff_sorted.csv. For this part, use any of your sorts.
-		//      The file will have one ASIN per line.
-		
+
+		// TODO: Sort based on averageRating from highest to lowest.
+
+		// TODO: Write to a new .csv file named whatever the file being read is
+		// with
+		// "_sorted"
+		// added to it. So if the file being read is called ratings_Stuff.csv,
+		// your file
+		// will be called ratings_Stuff_sorted.csv. For this part, use any of
+		// your
+		// sorts.
+		// The file will have one ASIN per line.
+
 		try {
 			// Requires both result and sorted files to be in current directory
 			Test.testFile(args);
